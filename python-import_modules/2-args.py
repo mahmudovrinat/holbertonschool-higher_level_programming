b7348305e77cf5b4c62c608e6f9855e59163c403
Rinat <mahmudovrinat88@gmail.com>
2025-06-20 13:43:59 +0000

    :gufbfkjebkjn

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    argc = len(argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print(f"{argc} arguments:")
+
+    for i in range(1, len(argv)):
+        print(f"{i}: {argv[i]}")
