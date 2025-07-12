d9dcbfb51f5fcef7398f380196d2400c7a8ec2f6
Rinat <mahmudovrinat88@gmail.com>
2025-07-12 14:00:37 +0000

    taskk

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """
-This script uses the GitHub API to list 10 most recent commits
-from a given repository and owner.
-Usage: ./100-github_commits.py <repository> <owner>
+This script fetches and prints the 10 most recent commits of a repository
+using the GitHub API.
+
+Usage:
+    ./100-github_commits.py <repository_name> <owner_name>
 """
 
 import requests
 import sys
+
 
 if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
 
+    response = requests.get(url)
     try:
-        response = requests.get(url)
         commits = response.json()
-
         for commit in commits[:10]:
             sha = commit.get("sha")
             author = commit.get("commit", {}).get("author", {}).get("name")
